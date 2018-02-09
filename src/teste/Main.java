c1ef353a19c58ccf4ca1e16890be443e2b41ad5d
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-02-08 22:23:27 -0200

    teste

--- src/teste/Main.java
+++ src/teste/Main.java
@@ -23,7 +23,7 @@ public class Main {
 		sms.setDestino("(83)998884411");
 		sms.setTexto("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
 				+ "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
-				+ "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+				+ "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...");
 		return sms;
 	}
 	
